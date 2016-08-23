b31abe2c18f5e253a06a36c851d3798066cfaec8
ricrol <rich.roller@gmail.com>
2016-08-23 08:00:48 -0400

    some updates

--- library/netmiko_command.py
+++ library/netmiko_command.py
@@ -2,12 +2,21 @@
 from importlib import import_module
 import logging
 import os
+import yaml
+
 
 try:
     import netmiko
     MEETS_REQUIREMENTS = True
 except ImportError:
     MEETS_REQUIREMENTS = False
+
+
+def convert_to_python_obj(string):
+    try:
+        return yaml.load(string)
+    except ValueError:
+        return False
 
 
 def execute_show_command(netmiko_object, command):
@@ -30,15 +39,35 @@ def setup_logging(args):
     if logfile is not None:
         logging.basicConfig(filename=logfile, level=logging.INFO,
                             format='%(asctime)s:%(name)s:%(message)s')
-        logging.getLogger().name = 'CONFIG:' + args['host']
+        logging.getLogger().name = 'CONFIG:'
 
 
 def setup_netmiko_connection(dev_params):
     try:
         return netmiko.ConnectHandler(**dev_params)
     except Exception as err:
-        logging.error("Exception: {}".format(err.message))
+        logging.error("Exception: {}".format(err.message), exc_info=True)
+
+
+def load_hosts_from_file(filename, root_path, delimeter, key):
+    hosts = set()
+    root_path = '.' if not(root_path) else root_path
+    delimeter = ',' if not(delimeter) else delimeter
+    key = 0 if not(key) else key
+    logging.info('{} {} {} {}'.format(filename, root_path, delimeter, key))
+
+    try:
+        with open(filename) as f:
+            for line in f:
+                host = line.strip().split(delimeter)[key]
+                hosts.add(host)
+    except IOError as err:
+        logging.error('File not found', exc_info=True)
         raise err
+    except IndexError:
+        logging.error('Key not found')
+
+    return list(hosts)
 
 
 def main():
@@ -46,7 +75,8 @@ def main():
     """
     module = AnsibleModule(
         argument_spec=dict(
-            host=dict(required=True),
+            host=dict(required=False, default=None),
+            host_file=dict(required=False, default=None),
             user=dict(required=False, default=os.getenv('USER')),
             passwd=dict(required=False, default=None),
             device_type=dict(required=False, default='cisco_ios'),
@@ -58,27 +88,38 @@ def main():
         ),
         supports_check_mode=False)
 
+    # create function to validate parameters
+
     if not MEETS_REQUIREMENTS:
         module.fail_json(msg='netmiko >= 0.1.3 is required for this module')
         exit(1)
 
     args = module.params
     setup_logging(args)
-    logging.info('here we be {}'.format(args))
     warnings = []
+    device_output = ''
+
+    if args['host_file']:
+        host_file = convert_to_python_obj(args['host_file'])
+        filename, root_path, delimeter, key = host_file[0], host_file[1:2], host_file[2:3], host_file[3:4]
+        hosts = load_hosts_from_file(filename, root_path, delimeter, key)
+    else:
+        hosts = [(args['host'])]
+
     dev_params = {"device_type": args['device_type'],
-                  "ip": args['host'],
                   "username": args['user'],
                   "password": args['passwd'],
                   "key_file": args['key_file'],
                   "verbose": False}
 
+    # snmp validation, yes for device_type
+    for host in hosts:
+        dev_params['ip'] = host
+        logging.info("connecting to {}.\nParameters:{}".format(host, dev_params))
+        netmiko_object = setup_netmiko_connection(dev_params)
+        device_output += '\n' + execute_show_command(netmiko_object, args['command'])
 
-
-    logging.info("connecting to {}.\nParameters:{}".format(args['host'], dev_params))
-    netmiko_object = setup_netmiko_connection(dev_params)
-    # snmp validation?
-    device_output = execute_show_command(netmiko_object, args['command'])
+    logging.info('Final output: {}'.format(device_output))
 
     if args['validate_module']:
         logging.info('args: {} {}'.format(args['validation_args'], args['validate_module']))
